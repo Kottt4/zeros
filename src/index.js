45893a366b9dc24bfe543f1f0cecc2462c62ea17
Kottt4 <vc@tut.by>
2018-09-22 10:28:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    var magicNumber = 1;
+    var november = 0;
+    var september = 0;
+    
+    while (magicNumber < number){
+    magicNumber *= 5;
+    september = Math.floor(number/magicNumber)
+    november += september;
+    };
+    
+    return november;
+  };
+
